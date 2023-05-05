aac397f723273c6892149ee5d1aacf94db4e11bd
mohamadreza <mohamadrezakhf@Gmail.com>
2023-05-05 16:32:18 +0430

    [complete] user model

--- src/module/auth/model/user.model.ts
+++ src/module/auth/model/user.model.ts
@@ -1,1 +1,25 @@
-export default class Comment {}
+import { ApiProperty } from '@nestjs/swagger'
+import { Role } from '@prisma/client'
+
+export default class UserModel {
+	@ApiProperty()
+	username: string
+
+	@ApiProperty()
+	email: string
+
+	@ApiProperty()
+	fullname: string
+
+	@ApiProperty()
+	role: Role
+
+	@ApiProperty()
+	isActive: string
+
+	@ApiProperty()
+	createdDate: Date
+
+	@ApiProperty()
+	updatedData: Date
+}
