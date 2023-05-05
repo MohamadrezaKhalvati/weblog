c661669150a253e83fad3fb5de4e0c5d31238587
mohamadreza <mohamadrezakhf@Gmail.com>
2023-05-05 16:39:54 +0430

    [complete] comment model

--- src/module/comment/model/comment.model.ts
+++ src/module/comment/model/comment.model.ts
@@ -1,1 +1,12 @@
-export default class Comment {}
+import { ApiProperty } from '@nestjs/swagger'
+
+export default class CommentModel {
+	@ApiProperty()
+	content: string
+
+	@ApiProperty()
+	authorId: string
+
+	@ApiProperty()
+	postId: string
+}
