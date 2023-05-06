ca3a5403a97900be04b0f5a68998efc8ef003f34
mohamadreza <mohamadrezakhf@Gmail.com>
2023-05-06 18:28:37 +0430

    [init] comment service

--- src/module/comment/comment.module.ts
+++ src/module/comment/comment.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from '../auth/auth.module'
+import { PostModule } from '../post/post.module'
 import { PrismaModule } from '../prisma/prisma.module'
 import { CommentController } from './comment.controller'
 import { CommentService } from './comment.service'
@@ -7,6 +8,6 @@ import { CommentService } from './comment.service'
 @Module({
 	controllers: [CommentController],
 	providers: [CommentService],
-	imports: [PrismaModule, AuthModule],
+	imports: [PrismaModule, AuthModule, PostModule],
 })
 export class CommentModule {}
--- src/module/comment/comment.service.ts
+++ src/module/comment/comment.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { AuthService } from '../auth/auth.service'
+import { PostService } from '../post/post.service'
+import { PrismaService } from '../prisma/prisma.service'
+import CreateCommentInput from './dto/create-comment.input'
+import DeleteCommentInput from './dto/delete-comment.input'
+import UpdateCommentInput from './dto/update-comment.input'
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+	constructor(
+		private prisma: PrismaService,
+		private authService: AuthService,
+		private PostService: PostService,
+	) {}
+
+	async createComment(input: CreateCommentInput) {}
+
+	async readComment(input: CreateCommentInput) {}
+
+	async updateComment(input: UpdateCommentInput) {}
+
+	async deleteComment(input: DeleteCommentInput) {}
+}
